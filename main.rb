7619ac1c443bd23613737ff3b3cba40cd17ea9fb
ld <lambdadelta@Koumakan.local>
2020-05-17 00:37:56 +0300

    add solution

--- main.rb
+++ main.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
+require 'csv'
+
 # read the string filename
 filename = gets.strip
+data = CSV.read(filename, col_sep: ' ')
+hash = Hash.new(0)
+
+data.each do |row|
+  timestamp = row[3][1..-1]
+  hash[timestamp] += 1
+end
+
+result = hash.select { |_, v| v > 1 }.keys
+
+File.open("req_#{filename}", 'w') do |file|
+  file.write result.join("\n")
+end
